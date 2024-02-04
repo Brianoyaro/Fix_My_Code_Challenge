3459ce10d02d7c48486a24f40aee1498f59b2e2b
Brianoyaro <brianoyaro2000@yahoo.com>
2024-02-04 07:16:00 +0300

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,11 +14,11 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PerimeterOfMySquare(self):
         """perimeter of square"""
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4) + (self.height * 0)
 
     def __str__(self):
         """nice square representation"""
